853d7dd0ddf7665e87c39d0c4d1cd0dbd92baf8d
Mahmoud Mohammed <progmahmoud95@gmail.com>
2018-08-12 21:25:33 +0200

    all views completed

---
+++ pixel-city/View/PhotoCell.swift
@@ -0,0 +1,19 @@
+//
+//  PhotoCell.swift
+//  pixel-city
+//
+//  Created by Mahmoud Mohammed on 8/11/18.
+//  Copyright © 2018 Mahmoud Mohammed. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implement")
+    }
+}
